26e2284c1cd0001e9bfb522adda650be8f01ddeb
Tiago Santos <tiago40427@gmail.com>
2025-06-25 22:44:21 -0300

    Exemplo simples de Classes
    Neste commit criei uma classe e exportei para instanciar em um JSX

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { InputName } from './Components/InputName'
+
+function App() {
+
+  
+  return (
+    <>
+      <InputName />    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/InputName/index.tsx
@@ -0,0 +1,34 @@
+import { Person } from "../../typescript";
+import { useState } from "react";
+
+export function InputName() {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+
+  function handleCreateName(){
+    if(!name)return alert('Type your name')
+
+    const person1 = new Person(name)
+    setUserName(person1.name)
+  }
+
+  return(
+    <>
+        <input
+        style={{marginBottom: "1rem"}}
+        type="text"
+        value={name}
+        onChange={e=>setName(e.target.value)}
+        placeholder="Digite seu nome"
+        /><br/>
+
+        <button onClick={handleCreateName}>Print</button>
+
+        {userName &&(
+            <div>
+                <h1>Hello {userName}!</h1>
+            </div>
+        )}
+    </>
+  )
+}
